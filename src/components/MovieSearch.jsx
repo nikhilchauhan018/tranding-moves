9e8f66f627db45d3a7e62d7c018b3cc973c953b5
nikhilkumarchauhan165@gmail.com <nikhilkumarchauhan165@gmail.com>
2025-07-11 22:23:23 +0530

    Initial commit

---
+++ src/components/MovieSearch.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const MovieSearch = ({ MovieSearchTerm, setMovieSearchTerm }) => {
+  return (
+    <div className="MovieSearch">
+      <div>
+        <img src="MovieSearch.svg" alt="MovieSearch" />
+
+        <input
+          type="text"
+          placeholder="MovieSearch through thousands of movies"
+          value={MovieSearchTerm}
+          onChange={(e) => setMovieSearchTerm(e.target.value)}
+        />
+      </div>
+    </div>
+  )
+}
+export default MovieSearch
